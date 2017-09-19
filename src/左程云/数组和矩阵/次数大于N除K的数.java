058cbc88f4969ad03d791b74ca0b459919087afd
zhutingting <18050102018@163.com>
2017-09-19 22:38:45 +0800

    得到次数大于N/K的数

---
+++ src/左程云/数组和矩阵/次数大于N除K的数.java
@@ -0,0 +1,100 @@
+package 左程云.数组和矩阵;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by yyc on 2017/9/19.
+ * 给定一个整型数组arr,打印其中出现次数大于一半的数，时间复杂度为O(N),额外空间复杂度为O(1)
+ * 进阶：给定一个整型数组arr和整数K，打印所有出现次数大于N/K的数，时间复杂度O(N*K),额外空间复杂度为O(K)
+ */
+public class 次数大于N除K的数 {
+    public static void printHalfMajor(int[] arr){
+        int cand=0;
+        int times=0;
+        for(int i=0;i!=arr.length;i++){
+            if(times==0){
+                cand=arr[i];
+                times=1;
+            }else if(arr[i]==cand){
+                times++;
+            }else {
+                times--;
+            }
+        }
+        times=0;
+        for(int i=0;i!=arr.length;i++){
+            if(arr[i]==cand){
+                times++;
+            }
+        }
+        if(times>arr.length/2){
+            System.out.println(cand);
+        }else {
+            System.out.println("no such number");
+        }
+    }
+
+    public static void printKMajor(int[] arr,int K){
+        if(K<2){
+            System.out.println("the value of K is invalid");
+        }
+        HashMap<Integer,Integer> cands=new HashMap<>();
+        for(int i=0;i!=arr.length;i++){
+            if(cands.containsKey(arr[i])){
+                cands.put(arr[i],cands.get(arr[i])+1);
+            }else {
+                if(cands.size()==K-1){
+                    allCandsMinusOne(cands);
+                }else {
+                    cands.put(arr[i],1);
+                }
+            }
+        }
+        HashMap<Integer,Integer> reals=getReals(arr,cands);
+        boolean hasPrint=false;
+        for(Map.Entry<Integer,Integer> set:cands.entrySet()){
+            Integer key=set.getKey();
+            if(reals.get(key)>arr.length/K){
+                hasPrint=true;
+                System.out.print(key+" ");
+            }
+        }
+        System.out.println(hasPrint ?"":"no such number");
+    }
+    public static void allCandsMinusOne(HashMap<Integer,Integer> map){
+        List<Integer> removeList=new LinkedList<Integer>();
+        for(Map.Entry<Integer,Integer> set:map.entrySet()){
+            Integer key=set.getKey();
+            Integer value=set.getValue();
+            if(value==1){
+                removeList.add(key);
+            }
+            map.put(key,value-1);
+        }
+        for(Integer removeKey:removeList){
+            map.remove(removeKey);
+        }
+    }
+    public static HashMap<Integer,Integer> getReals(int[] arr,HashMap<Integer,Integer>cands){
+        HashMap<Integer,Integer> reals=new HashMap<>();
+        for(int i=0;i!=arr.length;i++){
+            int curNum=arr[i];
+            if(cands.containsKey(curNum)){
+                if(reals.containsKey(curNum)){
+                    reals.put(curNum,reals.get(curNum)+1);
+                }else {
+                    reals.put(curNum,1);
+                }
+            }
+        }
+        return reals;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,1,2,2,3};
+        printHalfMajor(arr);
+        printKMajor(arr,3);
+    }
+}
